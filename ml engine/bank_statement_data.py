c08a15e4b9b734000f304bcdb92907024e0e910c
yash  yadav <yasxcode@gmail.com>
2025-07-25 23:53:25 +0530

    added bank statement dataframe

---
+++ ml engine/bank_statement_data.py
@@ -0,0 +1,20 @@
+# Install dependencies as needed:
+# pip install kagglehub[pandas-datasets]
+import pandas as pd
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+# Set the path to the file you'd like to load
+file_path = "bankstatements.csv"
+
+# Load the latest version
+df = kagglehub.load_dataset(
+  KaggleDatasetAdapter.PANDAS,
+  "abutalhadmaniyar/bank-statements-dataset",
+  file_path,
+  # Provide any additional arguments like 
+  # sql_query or pandas_kwargs. See the 
+  # documenation for more information:
+  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+)
+
